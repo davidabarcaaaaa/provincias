6f9e309003b5b86e712e3c504aab084a02e26962
David <david.abarcaherranz@usp.ceu.es>
2023-10-23 11:20:33 +0200

    ESPAÑA

---
+++ src/presentacion/Interfaz.java
@@ -0,0 +1,31 @@
+package presentacion;
+import dominio.*;
+import java.util.*;
+
+public class Interfaz{
+
+	// public static Arraylist <Provincia> leer {
+			ArrayList <Provincia> provincias = new Arraylist<> ();
+			Scanner sc = new Scanner (System.in);
+
+			do System.out.print ("Nombre de la provincia (enter para finalizar)")
+			String nombreprovincia = sc.nextLine();
+			if (nombreprovincia ! = ""){
+				provincia P = new Provincia (new Provincia);
+				do {
+					System.out.print ("Leer municipio de la provincia ");
+
+					Municipio m = new Municipio
+					p.add Municipio(m);
+				}
+				provincias.add(p);
+			} if (new Provincia ! "")
+		} while (new Provincia ! = "");
+		return provincias;
+		
+
+	} 
+
+
+}
+e 
